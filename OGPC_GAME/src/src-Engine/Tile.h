1ee3b27e58bfe173cc7f7c41a100827d92343903
NathanJewell <ncjewell@gmail.com>
2015-02-24 13:19:43 -0800

    decided to make engine less arbitrary

--- OGPC_GAME/src/src-Engine/Tile.h
+++ OGPC_GAME/src/src-Engine/Tile.h
@@ -5,18 +5,12 @@
 class Tile
 {
 public:
-    Tile(TileTemplate&);
+    Tile(ofTexture& texture);
     void update();
     void draw();
 
-    template<class T>
-    void setAttribute(T val, int indice)
-    {
-        attributes[indice] = val;
-    }
-
 private:
-    std::vector<tileAttribute> attributes;//TEXTURE IS ALWAYS THE FIRST ATTRIBUTE
+    ofTexture* texture
 
 };
 
--- OGPC_GAME/src/src-Engine/TileManager.cpp
+++ OGPC_GAME/src/src-Engine/TileManager.cpp
@@ -2,10 +2,7 @@
 
 TileManager::TileManager(std::string file)
 {
-    TileTemplate basicTile;//create temporary tile template
-    basicTile.addAttribute(ofTexture* texture, "Texture");// add Texture attribute
-    basicTile.addAttribute(int value, "Value");//add arbitrary value attribute
-    templates.push_back(basicTile);//push template into template array
+
 }
 
 
@@ -21,17 +18,17 @@ void TileManager::loadFromFile(std::string file)
         tileSize.y = theMap.getValue("tileSizeY", 0.0);
         int numOfTiles = mapSizeX*mapSizeY;
         theMap.pushTag("TILE");
-        for(int ii = 0; ii < numOfTiles; iii++)
+        for(int ii = 0; ii < numOfTiles; ii++)
         {
-            Tile tmp(templates[templateNames{tileType}]);
-            tmp.setAttribute()
+            Tile tmp(NULL);
+            tmp.setTexture(resources.getTextureRef(theMap.getValue("Texture", "")));;
+
             tiles.push_back(tmp);
         }
 
-        for(int ii = 0; ii < numOfTiles; iii++)
+        for(int ii = 0; ii < numOfTiles; ii++)
         {
-            Tile tmp(templates[0]);
-            tiles.push_back(tmp);
+
         }
 
 

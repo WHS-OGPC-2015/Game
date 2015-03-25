8d8f6c756f075d364e8d99215506c4c7da3fc8ca
Arccosecantism <riley.d.waugh@gmail.com>
2015-03-25 14:19:21 -0700

    worked a lot on the cities

--- OGPC_GAME/src/src-Engine/City.cpp
+++ OGPC_GAME/src/src-Engine/City.cpp
@@ -1,42 +1,42 @@
 #include "City.h"
 
 
-std::string randomname()
-{
-    std::vector <std::string> prefixes;
-    std::vector <std::string> suffixes;
-    std::string line;
-
-    int recordpf = 0;
-
-    std::ifstream TownNames("CityNames.txt");
-    if(TownNames.is_open())
-    {
-        while(getline(TownNames, line))
-        {
-            if (line == "---Prefixes---")
-            {
-                recordpf = 1;
-            }
-            else if (recordpf == 1)
-            {
-                prefixes.push_back(line);
-            }
-            if (line == "---Suffixes---")
-            {
-                recordpf = 2;
-            }
-            else if (recordpf == 2)
-            {
-                suffixes.push_back(line);
-            }
-        }
-        TownNames.close();
-    }
-    std::string retstr = prefixes[ofRandom(0, prefixes.size())]+suffixes[ofRandom(0,suffixes.size())];
-    return  retstr;
-}
-
+//std::string randomname()
+//{
+//    std::vector <std::string> prefixes;
+//    std::vector <std::string> suffixes;
+//    std::string line;
+//
+//    int recordpf = 0;
+//
+//    std::ifstream TownNames("CityNames.txt");
+//    if(TownNames.is_open())
+//    {
+//        while(getline(TownNames, line))
+//        {
+//            if (line == "---Prefixes---")
+//            {
+//                recordpf = 1;
+//            }
+//            else if (recordpf == 1)
+//            {
+//                prefixes.push_back(line);
+//            }
+//            if (line == "---Suffixes---")
+//            {
+//                recordpf = 2;
+//            }
+//            else if (recordpf == 2)
+//            {
+//                suffixes.push_back(line);
+//            }
+//        }
+//        TownNames.close();
+//    }
+//    std::string retstr = prefixes[ofRandom(0, prefixes.size())]+suffixes[ofRandom(0,suffixes.size())];
+//    return  retstr;
+//}
+//
 
 
 
@@ -51,43 +51,132 @@ City::City(int dif, Tile& btile) : boundTile(btile)
     TileIndex = boundTile;
     population = 1000; // default population
     converted = 0; // you don't have any influence
-    cityName = randomname(); // set name to a random combination of prefixes and suffixes
+   // cityName = randomname(); // set name to a random combination of prefixes and suffixes
     maxdif = 5;  // set the maximum difficulty for ALL cities
+    accelmax = 50;
     difficulty = dif;     // set the difficulty for a city
-    thresholds[0] = .05;     // below this percentage of believers in a discovered city, the believers will increase
-    thresholds[1] = .5 + difficulty; // above this
-    threshdolds[2] = .95;  // above this percentage in a discovered city, the converted will decline
+    thresholds[0] = .05;     // below this percentage of believers in an entered city, the believers will increase
+    thresholds[1] = .5 + difficulty; // above this percentage in an entered city, the converted will increase
+    thresholds[2] = .95;  // above this percentage in an entered city, the converted will decline
+    velmin = -(thresholds[1]-thresholdsd[0]) * accelmax / 2; //integral from bottom th to middle th of the acceleration, / (-2) -- the C of the integral
+    velmax = (thresholds[2]-thresholdsd[1]) * accelmax / 2;
+    extremeaccel[0] = 10;
+    extremeaccel[1] = -10;
+    threshnums[0] = 0;
+
 
 }
 
-void City::Update()
-{
-    if (percentconverted < thresholds[0])
+int City::getClickedData(ofVec2f& mousePos, bool& clicked, bool& pressed)
+ {
+    if (mousepos.x < TLpos.x || mousepos.x > BRpos.x ||
+        mousepos.y < TLpos.y || mouspos.y > BRpos.y)
     {
-       accel = 1;
-    }
-    if (percentconverted > thresholds[2])
-    {
-        accel = -1;
+        return 0;
+        // its ouside
     }
     else
     {
-        if (percentconverted < thresholds[1])
+        if (clicked == true)
         {
-           accel  = (percentconverted - thresholds[0])*(thresholds[1])
+            return 3;
+        }
+        else if (pressed == true)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+
+ }
+void City::Update(ofVec2f& mousePos, bool& clicked, bool& pressed)
+{
+    if (getClickedData(mousePos, clicked, pressed) == 1)
+    {
+        cityPopup->setActive();
+    }
+}
+
+
+void City::turnlyUpdate()
+{
+    percentconverted = converted / population;
+
+    if (entered == true)
+    {
+
+        threshnums[1] = threshnums[0];
+        if (percentconverted <= thresholds[0])
+        {
+           accel = extremeaccel[0];
+           threshnums[0] = 0;
+        }
+        else if (percentconverted >= thresholds[2])
+        {
+            accel = extremeaccel[1];
+            threshnums[0] = 3;
         }
         else
         {
 
+            if (percentconverted < thresholds[1])
+            {
+                threshnums[0] = 1;
+                accel  = (percentconverted - thresholds[0])*((accelmax)/(thresholds[1] - thresholds[0]));
+               //this is a point-slope formula
+            }
+            else
+            {
+                threshnums[0] = 2;
+                accel  = (percentconverted - thresholds[2])*((accelmax)/(thresholds[1] - thresholds[2]));
+                // this is, too
+            }
+        }
+//        if (percentconverted < thresholds[2])
+//        {
+//            add an acceleration based on the number of people in the city;
+//        }
+        if (threshnums[0] != threshnums[1]) // crossing thresholds
+        {
+            if (threshnums[1] == 0)
+            {
+                vel = velmin;
+            }
+            else if (threshnums[1] == 3)
+            {
+                vel = velmax;
+            }
+            else
+            {
+                vel = 0;
+            }
         }
 
+        vel += accel;
+        converted += vel;
+
+
+        if(converted < 0)
+        {
+            converted = 0;
+        }
+        if (converted > population)
+        {
+            converted = population;
+        }
     }
 
 
 
 
 
-    percentconverted = converted / population;
+
+
+
 }
 
 void City::Draw()
@@ -98,7 +187,7 @@ void City::Draw()
     }
     TLpos = ofVec2f(boundTile->position.x - cityTexture->getWidth() /2, boundTile->position.y - cityTexture->getHeight() /2);
     BRpos = ofVec2f(boundTile->position.x + cityTexture->getWidth() /2, boundTile->position.y + cityTexture->getHeight() /2);
-    cityPopup->draw(boundTile->position);
+    //cityPopup->draw(boundTile->position);
 
 
 }
@@ -116,6 +205,8 @@ void City::saveObjectData(ofxXmlSettings& file)
     file.addValue("population", population);
     file.addValue("converted", converted);
     file.addValue("velocity", velocity);
+    file.addValue("velmin", velmin);
+    file.addValue("velmax", velmax);
     file.addValue("accel", accel);
     file.addValue("TLposx", TLpos.x);
     file.addValue("TLposy", TLpos.y);
@@ -126,6 +217,7 @@ void City::saveObjectData(ofxXmlSettings& file)
     file.addValue("threshold2", thresholds[2]);
     file.addValue("difficulty", difficulty);
     file.addValue("occupied", occupied);
+    file.addValue("entered", entered);
 
 
 }
@@ -138,6 +230,8 @@ void City::loadObjectData(ofxXmlSettings& file);
     population = file.getValue("population", 0.0);
     converted = file.getValue("converted", 0.0);
     velocity = file.getValue("velocity", 0.0);
+    velmin = file.getValue("velmin", 0.0);
+    velmax = file.getValue("velmax", 0.0);
     accel = file.getValue("accel", 0.0);
     ofVec2f TLpos = ofVec2f(file.getValue("TLposx", 0.0), file.getValue("TLposy", 0.0));
     ofVec2f BRpos = ofVec2f(file.getValue("BRposx", 0.0), file.getValue("BRposy", 0.0));
@@ -146,4 +240,5 @@ void City::loadObjectData(ofxXmlSettings& file);
     threshdolds[2] = file.getValue("threshold2", 0.0);
     difficulty = file.getValue("difficulty", 0);
     occupied = file.getValue("occupied", 0);
+    entered = flie.getValue("entered", 0 /*hopefully works for bool?*/)
 }
--- OGPC_GAME/src/src-Engine/City.h
+++ OGPC_GAME/src/src-Engine/City.h
@@ -10,8 +10,11 @@ class City
 public:
     City(Tile& btile) : boundTile(btile);
 
-    void Update();
+    void Update(ofVec2f& mousePos, bool& clicked, bool& pressed);
+    void turnlyUpdate();
+    int getClickedData(ofVec2f& mousePos, bool& clicked, bool& pressed)
     void Draw();
+
 
     void saveObjectData(ofxXmlSettings&);
     void loadObjectData(ofxXmlSettings&);
@@ -25,20 +28,31 @@ private:
     std::string TextureName;
     std::string cityName;
     Menu* cityPopup;
+
     double population;
     double converted;
     double percentconverted;
+
     ofVec2f TLpos;
     ofVec2f BRpos;
-    double thresholds[3];
-    double converted;
+
+    double thresholds[3]; // th 0 and th 2 are same in all cities
+
+    // converted could also go here
     double velocity;
+    double velmin;
+    double velmax;
     double accel;
-    int occupied;
+    double accelmax;    //same in all cities
 
+    double extremeaccel[2]; // same in all cities
 
-    double
+    int threshnums[2];
+
+    int occupied;   // how many disciples are in the city
+    bool entered;
+
     int difficulty;
-    int maxdif;
+    int maxdif;     //same in all cities
 
 };

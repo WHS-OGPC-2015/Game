e485d1387fff40833fa8514795b6bc1cc1e1ea5b
Nathan <ncjewell@gmail.com>
2015-03-28 09:19:24 -0700

    Update LoadingScreen.h
    manually added loading screen update b/c of problems with implementation

--- OGPC_GAME/src/src-Game/LoadingScreen.h
+++ OGPC_GAME/src/src-Game/LoadingScreen.h
@@ -1,17 +1,15 @@
 #pragma once
 #include "ofMain.h"
-void draw()
-{
-    std::vector<
-}
+
 
 class rectObject
 {
 public:
-    rectObject(ofVec2f location, ofVec2f w)
+    rectObject(ofVec2f location, int w, int h)
     {
         position = location;
         width = w;
+        height = h;
     }
 
     void draw()
@@ -19,10 +17,6 @@ public:
         ofRect(position.x, position.y, width, height);
     }
 
-    void addLength(int W)
-    {
-        width+=w;
-    }
     int width, height;
     ofVec2f position;
 
@@ -31,30 +25,102 @@ public:
 class LoadingScreen
 {
 public:
-    LoadingScreen(int g, int n, int w)
+    LoadingScreen(int iterations, int s, ofVec2f start, int w)
     {
         screenSize = ofVec2f(ofGetWindowWidth(), ofGetWindowHeight());
-        gap = g;
-        num = n;
+        //gap = g;
+        //num = n;
         width = w;
-        currentWidth = 0;
-        currentSide = 1;  //1 = top 2 = right 3 = bottom 4= left;
+        direction = 1;  //1 = right 2 = left 3 = down 4= up;
+        speed = s;
+        maxIt = iterations;
         it = 2;
+        speed = 1;
+        pos = start;
     }
     void draw()
     {
+        ofBackground(79, 67, 44);
         ofSetColor(176, 136, 31);
         for(int ii = 0; ii < segs.size(); ii++)
         {
             segs[ii].draw();
         }
     }
-    void update(float Progress)
+    bool update()
     {
         if(first)
         {
-            randLength = ofRandom(ofGetWindowHeight()/(it-1), ofGetWindowHeight()/it);
+            if(segs.size() > 1)
+            {
+                if(segs[segs.size()-1].width == width)
+                {
+                    pos.y += segs[segs.size()-1].height;
+                }
+                else
+                {
+                    pos.x += segs[segs.size()-1].width;
+                }
+            }
+
+            it++;
+            randLength = ofRandom(ofGetWindowHeight()/it, ofGetWindowHeight()/(it-1));
+            direction = ofRandom(1, 3);
+
+            if(direction == 1)
+            {
+                rectObject temp(pos, width, 0);
+                segs.push_back(temp);
+            }
+            else if(direction == 2)
+            {
+                rectObject temp(pos, width, 0);
+                segs.push_back(temp);
+            }
+            else if(direction == 3)
+            {
+                rectObject temp(pos, 0, width);
+                segs.push_back(temp);
+            }
+            else if(direction == 4)
+            {
+                rectObject temp(pos, 0, width);
+                segs.push_back(temp);
+            }
+
+            first = false;
         }
+        if(segs[segs.size()-1].height < randLength ||
+           segs[segs.size()-1].width < randLength)
+        {
+            if(direction == 1)
+            {
+                segs[segs.size()-1].width += speed;
+            }
+            else if(direction == 2)
+            {
+                segs[segs.size()-1].width -= speed;
+            }
+            else if(direction == 3)
+            {
+                segs[segs.size()-1].height -= speed;
+            }
+            else if(direction == 4)
+            {
+                segs[segs.size()-1].height += speed;
+            }
+
+        }
+        else
+        {
+            first = true;
+        }
+        if(it == maxIt)
+        {
+            return false;
+        }
+        return true;
+
     }
 private:
     std::vector<rectObject> segs;
@@ -63,7 +129,12 @@ private:
     int num;
     int width;
     int currentIndent;
-    int currentSide;
+    int direction;
     int randLength;
-    int it;
+    int it;     //current iteration
+    int maxIt;  //maxinum number of iterations
+    bool first;
+    int speed;
+    ofVec2f pos;
 };
+

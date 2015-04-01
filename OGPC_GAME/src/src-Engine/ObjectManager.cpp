f3505bcc50b1bc6a69e2582cc331f743e021f888
NathanJewell <ncjewell@gmail.com>
2015-03-31 18:00:11 -0700

    need to pull

--- OGPC_GAME/src/src-Engine/ObjectManager.cpp
+++ OGPC_GAME/src/src-Engine/ObjectManager.cpp
@@ -1,90 +1,80 @@
-//#include "ObjectManager.h"
-//
-//
-////void ObjectManager::addObjectToType(auto object, std::string type)
-////{
-////    objects[objectArrayNames[type]].addObject[object];
-////}
-//
-////oVector ObjectManager::getObjectTypePointer(std::string type)
-////{
-////    return objects[objectArrayNames[type]];
-////}
-//
-//void ObjectManager::loadFromFile(std::string filePath)
-//{
-//    ofxXmlSettings objects;
-//    if(objects.loadFile(filePath))
-//    {
-//
-//        std::vector<std::string> objectNames;
-//        int numObjectTypes = objects.getValue("numObjectTypes", 0);
-//
-//        std::string type;
-//        for(int ii = 0; ii < numObjectTypes; ii++)
-//        {
-//            objects.pushTag("type", ii)
-//            type = objects.getValue("name", "");
-//            numType = object.getValue("numOf", 0);
-//            if(objectArrayNames.find(type) == objectArrayNames.end())
-//            {
-//                if(type == "City")
-//                {
-//                    addObjectType<City>("City");
-//                }
-//                else if(type == "Player")
-//                {
-//                    addObjectType<Player>("Player");
-//                }
-//                else if(type == "??????")
-//                {
-//                    //add
-//                }
-//                objects[objectArrayNames[type]].loadObjectData(objects, numType);   //load all objects of that type
-//            }
-//
-//
-//
-//        }
-//
-//    }
-//
-//}
-//
-//void ObjectManager::saveToFile(std::string path)
-//{
-//    ofxXmlSettings file;
-//    file.addValue("numObjectTypes", objectArrayNames.size())
-//    for(int ii = 0; ii < objectArrayNames.size())
-//    {
-//        objects.addTag("type");
-//        objects.pushTag("type", ii);
-//        //map<std::string, int>::iterator it = m.begin()+ii;
-//        objects.addValue("name", objectArrayNames[ii].first/*it->first*/)
-//        objects.addValue("numOf", )
-//        objects[ii].saveObjectData(file);
-//
-//
-//
-//        objects.popTag();
-//
-//    }
-//}
-//
-//void ObjectManager::updateAll()
-//{
-//    for(int ii = 0; ii < objects.size())
-//    {
-//        objects[ii].updateAll();
-//    }
-//}
-//
-//void ObjectManager::drawAll()
-//{
-//    for(int ii = 0; ii < objects.size())
-//    {
-//        objects[ii].drawAll();
-//    }
-//}
-//
-//
+#include "ObjectManager.h"
+
+void ObjectManager::loadFromFile(std::string filePath)
+{
+    ofxXmlSettings objects;
+    if(objects.loadFile(filePath))
+    {
+
+        std::vector<std::string> objectNames;
+        int numObjectTypes = objects.getValue("numObjectTypes", 0);
+
+        std::string type;
+        for(int ii = 0; ii < numObjectTypes; ii++)
+        {
+            objects.pushTag("type", ii)
+            type = objects.getValue("name", "");
+            numType = object.getValue("numOf", 0);
+            if(objectArrayNames.find(type) == objectArrayNames.end())           //if statements deduce type which is then loaded
+            {
+                if(type == "City")
+                {
+                    addObjectType<City>("City");
+                    getPointerToChildByName<City>("City")->loadObjectData(objects, numType);
+                }
+                else if(type == "Player")
+                {
+                    addObjectType<Player>("Player");
+                    getPointerToChildByName<Player>("Player")->loadObjectData(objects, numType);
+                }
+                else if(type == "??????")
+                {
+                    //add
+                }
+            }
+
+
+
+        }
+
+    }
+
+}
+
+void ObjectManager::saveToFile(std::string path)
+{
+    ofxXmlSettings file;
+    file.addValue("numObjectTypes", objectArrayNames.size())
+    for(int ii = 0; ii < objectArrayNames.size())
+    {
+        objects.addTag("type");
+        objects.pushTag("type", ii);
+        //map<std::string, int>::iterator it = m.begin()+ii;
+        objects.addValue("name", objectArrayNames[ii].first/*it->first*/)
+        objects.addValue("numOf", objectArrayNames[ii].getVector().size)
+        objects[ii].saveObjectData(file);
+
+
+
+        objects.popTag();
+
+    }
+}
+
+void ObjectManager::updateAll()
+{
+    for(int ii = 0; ii < objects.size())
+    {
+        objects[ii].updateAll();
+    }
+}
+
+void ObjectManager::drawAll()
+{
+    for(int ii = 0; ii < objects.size())
+    {
+        objects[ii].drawAll();
+    }
+}
+
+
--- OGPC_GAME/src/src-Engine/ObjectVector.cpp
+++ OGPC_GAME/src/src-Engine/ObjectVector.cpp
@@ -1,37 +1,43 @@
-//#include "ObjectVector.h"
-//
-//oVector::oVector()
-//{
-//
-//}
-//
-//void ObjectVector::loadObjectData(ofxXmlSettings& file, int numType)
-//{
-//    for(int ii = 0; ii < numType; ii++)
-//    {
-//        vec[ii].loadObjectData(file);
-//    }
-//}
-//
-//void ObjectVector::saveObjectData(ofxXmlSettings& file)
-//{
-//    for(int ii = 0; ii < vec.size(); ii++)
-//    {
-//        vec[ii].saveObjectData(file);
-//    }
-//}
-//
-//std::vector<T> getVector()
-//{
-//    return vec;
-//}
-//
-//void updateAll()
-//{
-//
-//}
-//
-//void drawAll()
-//{
-//
-//}
+#include "ObjectVector.h"
+
+oVector::oVector()
+{
+
+}
+
+void ObjectVector::loadObjectData(ofxXmlSettings& file, int numType)
+{
+    for(int ii = 0; ii < numType; ii++)
+    {
+        vec[ii].loadObjectData(file);
+    }
+}
+
+void ObjectVector::saveObjectData(ofxXmlSettings& file)
+{
+    for(int ii = 0; ii < vec.size(); ii++)
+    {
+        vec[ii].saveObjectData(file);
+    }
+}
+
+std::vector<T> getVector()
+{
+    return vec;
+}
+
+void updateAll()
+{
+    for(int ii = 0; ii < vec.size(); ii++)
+    {
+        vec[ii].update();
+    }
+}
+
+void drawAll()
+{
+        for(int ii = 0; ii < vec.size(); ii++)
+    {
+        vec[ii].draw();
+    }
+}
